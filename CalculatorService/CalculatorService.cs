e25232021e7fe1ecdb6ee1c40676d99b9b8d30cb
DimonixR <repnev-dmitrii@yandex.ru>
2022-08-24 16:09:16 +0300

    Add project files.

---
+++ CalculatorService/CalculatorService.cs
@@ -0,0 +1,64 @@
+﻿namespace Service
+{
+    public class CalculatorService
+    {
+        private int _sumParellelForEach = 0;
+        private object _lockParellelForEach = new object();
+        private int _sumParellelThreadPool = 0;
+        private object _lockParellelThreadPool = new object();
+        CountdownEvent _countdownEventThreadPool;
+        public int Sum(IEnumerable<int> collection) => collection.Sum();
+
+        public int SumAsParellelForEach(IEnumerable<int> collection)
+        {
+            _sumParellelForEach = 0;
+            Parallel.ForEach(collection, HandlerParellelForEach);
+            return _sumParellelForEach;
+        }
+
+        public int SumAsParellelTask(IEnumerable<int> collection, int countThread)
+        {
+            var parts = SplitCollection(collection, countThread);
+            var tasks = parts.Select(p => Task.Run(() => Sum(p))).ToArray();
+            return tasks.Select(t => t.Result).Sum();
+        }
+
+        public int SumAsParellelThreadPool(IEnumerable<int> collection, int countThread)
+        {
+            _sumParellelThreadPool = 0;
+            _countdownEventThreadPool = new CountdownEvent(countThread);
+
+            for (int i = 0; i < countThread; i++)
+            {
+                ThreadPool.QueueUserWorkItem(HandlerParellelThreadPool, collection.Where((_, ind) => ind % countThread == i).ToArray());
+            }
+            _countdownEventThreadPool.Wait();
+            return _sumParellelThreadPool;
+        }
+
+        private void HandlerParellelThreadPool(object obj)
+        {
+            lock (_lockParellelThreadPool)
+            {
+                _sumParellelThreadPool += Sum((IEnumerable<int>)obj);
+            }
+            _countdownEventThreadPool.Signal();
+        }
+       
+        private void HandlerParellelForEach(int item)
+        {
+            lock (_lockParellelForEach)
+            {
+                _sumParellelForEach += item;
+            }
+        }
+
+        private IEnumerable<IEnumerable<int>> SplitCollection(IEnumerable<int> collection, int countThread)
+        {
+            int i = 0;
+            return collection.GroupBy(x => i++ % countThread);
+        }
+
+        public int SumLinq(IEnumerable<int> collection) => collection.AsParallel().Sum();
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Service;
+using System.Diagnostics;
+
+var sizeArray = 10_000_000;
+
+var arrayInt = Enumerable.Repeat(1, sizeArray);
+
+var calculator = new CalculatorService();
+
+Execute(() => calculator.Sum(arrayInt), "Normal calculation");
+Execute(() => calculator.SumAsParellelForEach(arrayInt), "ParallelForEach calculation");
+Execute(() => calculator.SumAsParellelThreadPool(arrayInt, 5), "Parallel ThreadPool calculation");
+Execute(() => calculator.SumAsParellelTask(arrayInt, 5), "Parallel Task calculation");
+Execute(() => calculator.SumLinq(arrayInt), "Linq calculation");
+
+static void Execute(Func<int> func, string nameExecutor)
+{
+    var sw = new Stopwatch();
+    sw.Start();
+    var result = func();
+    sw.Stop();
+    Console.WriteLine($"{nameExecutor} execute time => {sw.ElapsedMilliseconds} ms, result {result}");
+} 
